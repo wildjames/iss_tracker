86a2adada94b87f13185817378ab6ce72693d01c
wildjames <jwild2@sheffield.ac.uk>
2020-01-22 21:35:30 +0000

    Fixing arbitrary angles

--- stepper.py
+++ stepper.py
@@ -92,12 +92,13 @@ class stepMotors:
 
     def to_angle(self, desired_angle, direction=None):
         '''Desired angle is in degrees'''
+        desired_angle = desired_angle % 360
+
         print("I am at {}, and want to move to {}".format(self.location, desired_angle))
         if direction is None:
             self.direction = -1 if self.location > desired_angle else +1
         print("I'll move in the direction {}".format(self.direction))
 
-        desired_angle = desired_angle % 360
         self._desired_angle = desired_angle
 
         if self.state:
