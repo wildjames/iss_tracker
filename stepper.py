8b0f1a36134aee6b84b62fc0de0842c25dd446f7
wildjames <jwild2@sheffield.ac.uk>
2020-01-22 21:54:17 +0000

    Fixing arbitrary angles

--- stepper.py
+++ stepper.py
@@ -98,7 +98,7 @@ class stepMotors:
         if direction is None:
             dist = self.angle - desired_angle
             while dist >= 180:
-                dist -= 180
+                dist -= 360
             print("Distance is {}".format(dist))
             self.direction = -1 if dist > 0 else +1
         print("I'll move in the direction {}".format(self.direction))
