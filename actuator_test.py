3bc7a6fe20cd11d034572f9eb82e424e18b5a40b
wildjames <jwild2@sheffield.ac.uk>
2020-01-23 17:33:32 +0000

    Test the new servo class

--- actuator_test.py
+++ actuator_test.py
@@ -1,11 +1,16 @@
-from gpiozero import Servo, OutputDevice
-from time import sleep
-from stepper import stepMotors
-import pigpio
 import itertools
 import time
+from time import sleep
+
 from numpy.random import randint
 
+from servo import Servo
+from stepper import stepMotors
+
+# Datasheet: https://www.bitsbox.co.uk/data/motor/sg90.pdf
+min_pw = 1e-3
+max_pw = 2e-3
+frame  = 20e-3
 
 servo = Servo(14)
 
@@ -15,17 +20,22 @@ move = stepMotors(stepper_pins)
 DELAY = 5 # seconds
 
 stepper_angle = 0
-while True:
-    stepper_angle += 90
-    print("Moving to {} deg".format(stepper_angle))
-    move.to_angle(stepper_angle)
-    servo.min()
-    time.sleep(DELAY)
+try:
+    while True:
+        stepper_angle += 90
+        print("Moving to {} deg".format(stepper_angle))
+        move.to_angle(stepper_angle)
+        servo.value = stepper_angle % 180
 
-    stepper_angle += 90
-    print("Moving to {} deg".format(stepper_angle))
-    move.to_angle(stepper_angle)
-    servo.max()
-    time.sleep(DELAY)
+        time.sleep(DELAY)
 
 
+        stepper_angle += 90
+        print("Moving to {} deg".format(stepper_angle))
+        move.to_angle(stepper_angle)
+        servo.value = stepper_angle % 180
+
+        time.sleep(DELAY)
+
+except KeyboardInterrupt:
+    servo.close()
